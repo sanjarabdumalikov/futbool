c3d6d4adf58a7109c37ca2bd10b73c80590ca68a
sanjarabdumalikov <abdumalikovsancho@gmail.com>
2023-07-28 12:00:56 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -30,10 +30,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
